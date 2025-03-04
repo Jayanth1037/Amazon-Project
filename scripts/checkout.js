0ae544427e1a4a577c5d2591da80f5e709f91ed7
JayanthAnala <avvjayanth@gmail.com>
2025-03-03 20:20:34 -0500

    Created OOP version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderCheckoutHeader} from './chechout/checkoutHeader.js';
 import { renderOrderSummary } from './chechout/orderSummary.js';
 import { renderPaymentSummary } from './chechout/paymentSummary.js';
+import '../data/cart-oop.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
